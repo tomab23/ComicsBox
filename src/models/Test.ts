10480dcc9cbbbdbcdc3535645df4a4664e930d40
thomas bartier <thomas.bartier@gmail.com>
2025-09-08 13:20:32 +0200

    update Test & update homepage idea

--- src/models/Test.ts
+++ src/models/Test.ts
@@ -3,17 +3,20 @@ export default class Test{
     name: string;
     vo: boolean;
     page: number;
+    reading: boolean;
 
     constructor(
         id: number,
         name: string,
         vo: boolean,
         page: number,
+        reading: boolean,
     ){
         this.id = id;
         this.page = page;
         this.vo = vo;
         this.name = name;
+        this.reading = reading;
     }
 
 
--- src/models/TestList.ts
+++ src/models/TestList.ts
@@ -2,33 +2,38 @@ import type Test from "./Test"
 
 export const TestList : Test[] = [
  {
-    id: 1,
-    name: "Batman #1",
-    page: 25,
-    vo: true
+     id: 1,
+     name: "Batman #1",
+     page: 25,
+     vo: true,
+     reading: true
  },
   {
-    id: 2,
-    name: "Duke",
-    page: 165,
-    vo: false
- },
+      id: 2,
+      name: "Duke",
+      page: 165,
+      vo: false,
+      reading: false
+  },
   {
-    id: 3,
-    name: "Kick-ass",
-    page: 650,
-    vo: false
- },
+      id: 3,
+      name: "Kick-ass",
+      page: 650,
+      vo: false,
+      reading: true
+  },
   {
-    id: 4,
-    name: "Geiger #8",
-    page: 25,
-    vo: true
- },
+      id: 4,
+      name: "Geiger #8",
+      page: 25,
+      vo: true,
+      reading: false
+  },
   {
-    id: 5,
-    name: "Superman",
-    page: 200,
-    vo: false
- },
+      id: 5,
+      name: "Superman",
+      page: 200,
+      vo: false,
+      reading: false
+  },
 ]
