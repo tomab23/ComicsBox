99d81d9b47a7cfe263b7d412e3539c472e3ad86d
thomas bartier <thomas.bartier@gmail.com>
2025-09-10 14:02:13 +0200

    add validation icon card

--- src/components/cards/CardTest.tsx
+++ src/components/cards/CardTest.tsx
@@ -1,36 +1,45 @@
 import {
   Card,
   CardContent,
-//   CardFooter,
+  //   CardFooter,
   CardHeader,
 } from "@/components/ui/card";
 import type Test from "@/models/Test";
 import VersionBadge from "../VersionBadge";
-import { Eye } from "lucide-react";
+import { Check, Eye } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
-    test: Test
-}
-const CardTest = ({ test } : Props) => {
-    const navigate = useNavigate();
+  test: Test;
+};
+const CardTest = ({ test }: Props) => {
+  const navigate = useNavigate();
   return (
-        <Card className="max-w-xs w-60 shadow-none gap-0 pt-0">
+    <Card className="max-w-xs w-60 shadow-none gap-0 pt-0">
       <CardHeader className="py-4 px-5 font-semibold">
-            <p>{test.name}</p>
+        <p>{test.name}</p>
       </CardHeader>
       <CardContent className="text-sm text-muted-foreground px-5 flex justify-between">
         <div className="flex flex-row items-center gap-2">
-                    <p>{test.page} pages</p>
-        <VersionBadge vo={test.vo} />
+          <p>{test.page} pages</p>
+          <VersionBadge vo={test.vo} />
         </div>
-        <Eye className="cursor-pointer w-5" onClick={() => navigate(`/comic/${test.id}`)} />
+        <div className="flex gap-2">
+          <Check
+            className="cursor-pointer w-5 hover:scale-105"
+            onClick={() => alert("validation de lecture")}
+          />
+          <Eye
+            className="cursor-pointer w-5 hover:scale-105"
+            onClick={() => navigate(`/comic/${test.id}`)}
+          />
+        </div>
       </CardContent>
       {/* <CardFooter>
         footer
       </CardFooter> */}
     </Card>
-  )
-}
+  );
+};
 
-export default CardTest
+export default CardTest;
