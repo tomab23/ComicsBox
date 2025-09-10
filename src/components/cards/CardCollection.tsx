712703eb0f01ba4a97c40966acd1f8987a0b7c4a
thomas bartier <thomas.bartier@gmail.com>
2025-09-10 15:02:02 +0200

    add card comic collection

--- src/components/cards/CardCollection.tsx
+++ src/components/cards/CardCollection.tsx
@@ -1,38 +1,36 @@
 import { Book } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
-  border: string
-}
+  border: string;
+};
 
-const CardCollection = ({ border } : Props) => {
+const CardCollection = ({ border }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <Card className={`${border} w-full shadow-none gap-0 pt-0 hover:cursor-pointer dark:hover:border-yellow-400  hover:border-gray-800`}
-    onClick={() => navigate("/collection")}>
+    <Card
+      className={`${border} w-full hover:cursor-pointer shadow-none gap-0 pt-0 hover:dark:hover:border-yellow-400  hover:border-gray-800`}
+      onClick={() => navigate("/collection")}
+    >
       <CardHeader className="py-4 px-5 font-semibold">
-        <p>name (3)</p>
+        <p>
+          Collection VO <span className="italic text-sm">(10)</span>
+        </p>
       </CardHeader>
       <CardContent className="text-sm text-muted-foreground px-5">
         <div className="flex flex-row items-center gap-2">
-          <Book
-            className="cursor-pointer w-5 hover:scale-105"
-            onClick={() => alert("validation de lecture")}
-          />
-          <Book
-            className="cursor-pointer w-5 hover:scale-105"
-            onClick={() => alert("validation de lecture")}
-          />
-          <Book
-            className="cursor-pointer w-5 hover:scale-105"
-            onClick={() => alert("validation de lecture")}
-          />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
+          <Book className="w-5 hover:scale-105" />
         </div>
       </CardContent>
     </Card>
--- src/pages/CollectionPage.tsx
+++ src/pages/CollectionPage.tsx
@@ -1,19 +1,20 @@
-import ButtonBack from "@/components/buttons/ButtonBack"
-import Navbar from "@/components/layout/Navbar"
-import { Button } from "@/components/ui/button"
-import { Book } from "lucide-react"
+import ButtonBack from "@/components/buttons/ButtonBack";
+import CardComicCollection from "@/components/cards/CardComicCollection";
+import Navbar from "@/components/layout/Navbar";
+import { Button } from "@/components/ui/button";
+import { Book } from "lucide-react";
 
 const CollectionPage = () => {
   return (
     <div className="setpage">
-        <Navbar />
+      <Navbar />
       <div className="contenu">
         <ButtonBack />
         {/* HEADER */}
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold">Comics de la collection</h1>
           <Button
-          variant={"outline"}
+            variant={"outline"}
             className=""
             // onClick={() => navigate("/collection-form")}
           >
@@ -22,9 +23,26 @@ const CollectionPage = () => {
             Modifier
           </Button>
         </div>
+
+        {/* COMICS */}
+        <div className="mt-5">
+          <p className="text-xl">Vos comics</p>
+          <div className="mt-5 grid grid-cols-3 xl:grid-cols-5 lg:grid-cols-4 max-md:grid-cols-2 max-sm:gap-x-2 gap-y-3">
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+            <CardComicCollection />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollectionPage
+export default CollectionPage;
